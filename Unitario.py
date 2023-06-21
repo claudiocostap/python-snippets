37bcfb8439a1b512bcece9f0fec27170da5e5ab9
Claudio Costa de Paula <claudiocp.ads@gmail.com>
2023-06-21 15:20:03 -0300

    Update Unitario.py

--- Unitario.py
+++ Unitario.py
@@ -1,3 +1,48 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from module import process_notification
+
+@pytest.fixture
+def mock_envia_email():
+    with patch("module.envia_email") as mock:
+        yield mock
+
+@pytest.fixture
+def mock_insert_registro_email():
+    with patch("module.insert_registro_email") as mock:
+        yield mock
+
+@pytest.fixture
+def mock_update_registro_email():
+    with patch("module.update_registro_email") as mock:
+        yield mock
+
+@pytest.fixture
+def mock_logger():
+    with patch("module.logger") as mock:
+        yield mock
+
+def test_process_notification_success(mock_logger, mock_update_registro_email, mock_insert_registro_email, mock_envia_email):
+    # Configurando o mock do método envia_email
+    mock_envia_email.return_value.status_code = 202
+
+    dados = {
+        "num_ota": "123456",
+        "status": 2
+    }
+
+    process_notification(dados)
+
+    # Asserts do teste
+
+if __name__ == "__main__":
+    pytest.main()
+
+
+
+
+
+
 import unittest
 from unittest.mock import patch, MagicMock
 from module import process_notification
