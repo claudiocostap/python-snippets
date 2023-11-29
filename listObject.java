775387fdeec2ced22d1f1a6a1d1d22c5bea2a471
Claudio Costa de Paula <claudiocp.ads@gmail.com>
2023-11-29 09:57:18 -0300

    Update listObject.java

--- listObject.java
+++ listObject.java
@@ -1,10 +1,10 @@
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
-
-import java.util.List;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class ListS3Objects {
 
@@ -13,36 +13,30 @@ public class ListS3Objects {
         String accessKey = "SEU_ACCESS_KEY";
         String secretKey = "SEU_SECRET_KEY";
 
+        // Defina a região
+        String region = "us-east-1"; // Substitua pela sua região
+
         // Defina o nome do bucket
         String bucketName = "SEU_BUCKET_NAME";
 
-        // Crie o cliente S3
-        S3Client s3Client = S3Client.builder()
-                .region(Region.US_EAST_1) // Substitua pela sua região
-                .credentialsProvider(() -> AwsBasicCredentials.create(accessKey, secretKey))
+        // Crie o cliente AmazonS3
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://s3." + region + ".amazonaws.com", region))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
                 .build();
 
         // Liste os objetos no bucket
         listObjects(s3Client, bucketName);
     }
 
-    private static void listObjects(S3Client s3Client, String bucketName) {
-        ListObjectsV2Response listObjectsResponse = s3Client.listObjectsV2(builder ->
-                builder.bucket(bucketName)
-        );
+    private static void listObjects(AmazonS3 s3Client, String bucketName) {
+        ObjectListing objectListing = s3Client.listObjects(bucketName);
 
-        List<S3Object> objects = listObjectsResponse.contents();
-
-        if (objects.isEmpty()) {
-            System.out.println("O bucket está vazio.");
-        } else {
-            System.out.println("Objetos no bucket:");
-            for (S3Object object : objects) {
-                System.out.println("Nome do objeto: " + object.key());
-                System.out.println("Tamanho: " + object.size());
-                System.out.println("Última modificação: " + object.lastModified());
-                System.out.println("---");
-            }
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            System.out.println("Nome do objeto: " + objectSummary.getKey());
+            System.out.println("Tamanho: " + objectSummary.getSize());
+            System.out.println("Última modificação: " + objectSummary.getLastModified());
+            System.out.println("---");
         }
     }
 }
